e40f95d0c3c31aeaa03dd431fba54f49aeaecfda
hany <hani.badr@gizasystems.onmicrosoft.com>
2025-03-20 17:10:00 +0200

    rename plus third commit

--- src/Hany.java
+++ src/Hany.java
@@ -1,6 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Hany {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
@@ -13,6 +13,7 @@ public class Main {
         }
         System.out.printf("initial commit");
         System.out.printf("Second Commit ");
+        System.out.printf("third commit ");
 
     }
 }
